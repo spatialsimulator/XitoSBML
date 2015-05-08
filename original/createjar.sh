ec9cda301a0d788310fccdc60c5539ae777410dd
kaitoii11 <kaitoii1111@gmail.com>
2015-05-08 19:11:00 +0900

    removed unrelated files

--- original/createjar.sh
+++
@@ -1,1 +0,0 @@
-/bin/cp ./plugins.config ./src/ISight_Capture.java ./bin/ISight_Capture.class ./zip && cd zip && LC_ALL="C" jar cvf ISight_Capture.jar plugins.config *.class *.java
--- plug/build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-/bin/mv ./Spatial_SBML.jar ./libsbmlj.jar /Applications/Fiji.app/plugins
-/bin/mv ./libsbml.5.11.0.dylib ./libsbmlj.jnilib /Applications/Fiji.app
-/usr/bin/patch /Applications/Fiji.app/Contents/Info.plist < info.patch
