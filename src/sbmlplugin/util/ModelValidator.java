751bb54141804818b736e54badd244998fbc7486
kaitoii11 <kaitoii1111@gmail.com>
2015-11-17 19:19:06 +0900

    change plugin name, optimized model validator

--- src/sbmlplugin/util/ModelValidator.java
+++ src/sbmlplugin/util/ModelValidator.java
@@ -17,39 +17,10 @@ package sbmlplugin.util;
 
 import ij.IJ;
 
-import org.sbml.libsbml.AdvectionCoefficient;
-import org.sbml.libsbml.AnalyticGeometry;
-import org.sbml.libsbml.BoundaryCondition;
-import org.sbml.libsbml.Compartment;
-import org.sbml.libsbml.CoordinateComponent;
-import org.sbml.libsbml.DiffusionCoefficient;
-import org.sbml.libsbml.Geometry;
-import org.sbml.libsbml.GeometryDefinition;
-import org.sbml.libsbml.ListOf;
-import org.sbml.libsbml.ListOfAdjacentDomains;
-import org.sbml.libsbml.ListOfCompartments;
-import org.sbml.libsbml.ListOfCoordinateComponents;
-import org.sbml.libsbml.ListOfDomainTypes;
-import org.sbml.libsbml.ListOfDomains;
-import org.sbml.libsbml.ListOfGeometryDefinitions;
-import org.sbml.libsbml.ListOfParameters;
-import org.sbml.libsbml.ListOfReactions;
-import org.sbml.libsbml.ListOfSampledFields;
-import org.sbml.libsbml.ListOfSpecies;
 import org.sbml.libsbml.Model;
-import org.sbml.libsbml.Parameter;
-import org.sbml.libsbml.ParametricGeometry;
-import org.sbml.libsbml.Reaction;
 import org.sbml.libsbml.SBMLDocument;
 import org.sbml.libsbml.SBMLReader;
-import org.sbml.libsbml.SBase;
-import org.sbml.libsbml.SampledFieldGeometry;
-import org.sbml.libsbml.SpatialCompartmentPlugin;
-import org.sbml.libsbml.SpatialModelPlugin;
-import org.sbml.libsbml.SpatialParameterPlugin;
-import org.sbml.libsbml.SpatialSpeciesPlugin;
-import org.sbml.libsbml.SpatialSymbolReference;
-import org.sbml.libsbml.Species;
+import org.sbml.libsbml.SBMLValidator;
 
 
 public class ModelValidator {
@@ -62,14 +33,14 @@ public class ModelValidator {
 		}
 	}
 	
+	private SBMLDocument document;
 	private Model model;
-	private Boolean errorFlag = false;
-	private SpatialModelPlugin spatialplugin;
-	private boolean hasRequiredAttribute;
+
 	
-	public ModelValidator(Model model){
-		this.model = model;
-		 hasRequiredAttribute = checkModelVersion() && checkExtension();
+	public ModelValidator(SBMLDocument document){
+		this.document = document;
+		this.model = document.getModel();
+		
 	}
 	
 	private boolean checkModelVersion(){
@@ -80,188 +51,32 @@ public class ModelValidator {
 	}
 	
 	private boolean checkExtension(){
-		SBMLDocument document = model.getSBMLDocument();
 		
 		if(!document.getPackageRequired("spatial"))
 			IJ.log("model missing extension spatial");
-		 else
-		    spatialplugin =(SpatialModelPlugin) (model.getPlugin ("spatial"));   
 		
 		if(!document.getPackageRequired("req"))
 			IJ.log("model missing extension req");
 		
 		return document.getPackageRequired("spatial") && document.getPackageRequired("req");
 	}
-	
+
 	public void validate(){
+		boolean hasRequiredAttribute = checkModelVersion() && checkExtension();
+		
 		if(!hasRequiredAttribute)
 			return;
 		
-		checkModel(model);
-		checkSpecies(model.getListOfSpecies());
-		checkParameter(model.getListOfParameters());
-		checkCompartment(model.getListOfCompartments());
-		checkReaction(model.getListOfReactions());
-		checkGeometry();
-	
-		if(!errorFlag)
-			IJ.log( model.getId() + " Model is valid");
-		
-		IJ.log("Done checking model");
+		SBMLValidator sv = new SBMLValidator();
+		sv.validate(document);
+		IJ.log(sv.getErrorLog().toString());
 	}
 	
-	private void checkModel(Model model){
-		IJ.log("Checking model");
-		checkRequired(model);
-	}
-	
-	private void checkSpecies(ListOfSpecies los){
-		IJ.log("Checking species");
-		for(int i = 0 ; i < los.size() ; i++){
-			Species s = los.get(i);
-			checkRequired(s);
-			SpatialSpeciesPlugin ssp = (SpatialSpeciesPlugin) s.getPlugin("spatial");
-			ssp.hasRequiredElements();
-		}
-	}
-	
-	
-	private void checkParameter(ListOfParameters lop){
-		IJ.log("Checking parameter");
-		for(int i = 0 ; i < lop.size() ; i++){
-			Parameter p = lop.get(i);
-			SpatialParameterPlugin sp = (SpatialParameterPlugin) p.getPlugin("spatial");
-			checkRequired(p);
-			if(sp.isSpatialParameter())
-				checkSpatialParameter(sp);
-		}	
-	}
-	
-	private void checkSpatialParameter(SpatialParameterPlugin sp){
-		if(sp.isSetAdvectionCoefficient()){
-			AdvectionCoefficient ac = sp.getAdvectionCoefficient();
-			checkRequired(ac);
-		} else if(sp.isSetBoundaryCondition()){
-			BoundaryCondition bc = sp.getBoundaryCondition();
-			checkRequired(bc);
-		} else if(sp.isSetDiffusionCoefficient()){
-			DiffusionCoefficient dc = sp.getDiffusionCoefficient();
-			checkRequired(dc);
-		} else if(sp.isSetSpatialSymbolReference()){
-			SpatialSymbolReference ssr = sp.getSpatialSymbolReference();
-			checkRequired(ssr);
-		} else{
-			IJ.log("missing spatial in " + sp.getParentSBMLObject().getId() + " at line:" + sp.getLine());
-		}
-	}
-	
-	private void checkCompartment(ListOfCompartments loc){
-		IJ.log("Checking compartment");
-		for(int i = 0 ; i < loc.size() ; i++){
-			Compartment c = loc.get(i);
-			checkRequired(c);
-			SpatialCompartmentPlugin scp = (SpatialCompartmentPlugin) c.getPlugin("spatial");
-			checkSpatialCompartment(scp);
-		}
-	}
-	
-	private void checkSpatialCompartment(SpatialCompartmentPlugin scp){
-		if(scp.isSetCompartmentMapping()) checkRequired(scp.getCompartmentMapping());
-		else IJ.log("Missing compartment mapping in " + scp.getParentSBMLObject().getId() + " at line:" + scp.getLine());
-	}
-	
-	private void checkReaction(ListOfReactions lor){
-		IJ.log("Checking reaction");
-		for(int i = 0; i < lor.size() ; i++){
-			Reaction r = lor.get(i);
-			checkRequired(r);
-			checkList(r.getListOfReactants());
-			checkList(r.getListOfProducts());
-			checkList(r.getListOfModifiers());
-			checkRequired(r.getKineticLaw());
-			checkList(r.getKineticLaw().getListOfLocalParameters());
-		}
-	}
-		
-	private void checkGeometry(){
-		IJ.log("Checking geometry");
-		Geometry geometry = spatialplugin.getGeometry();
-		checkRequired(geometry);
-		checkCoordinateComponents(geometry.getListOfCoordinateComponents());
-		checkDomainType(geometry.getListOfDomainTypes());
-		checkDomains(geometry.getListOfDomains());
-		checkAdjacentDomains(geometry.getListOfAdjacentDomains());
-		checkGeometryDefinitions(geometry.getListOfGeometryDefinitions());
-		checkSampledField(geometry.getListOfSampledFields());
-	}
-	
-	private void checkSampledField(ListOfSampledFields losf){
-		checkList(losf);
-	}
-	
-	private void checkGeometryDefinitions(ListOfGeometryDefinitions logd){
-		for(int i = 0 ; i < logd.size() ; i++){
-			GeometryDefinition gd = logd.get(i);
-			checkRequired(gd);
-			if(gd instanceof AnalyticGeometry)
-				checkList(((AnalyticGeometry) gd).getListOfAnalyticVolumes());
-			
-			if(gd instanceof SampledFieldGeometry)
-				checkList(((SampledFieldGeometry) gd).getListOfSampledVolumes());
-		
-			if(gd instanceof ParametricGeometry){
-				checkRequired(((ParametricGeometry) gd).getSpatialPoints());
-				checkList(((ParametricGeometry) gd).getListOfParametricObjects());
-			}			
-		}
-	}
-	
-	private void checkAdjacentDomains(ListOfAdjacentDomains load){
-		checkList(load);
-	}
-	
-	private void checkDomains(ListOfDomains lod){
-		checkList(lod);
-	}
-	
-	private void checkDomainType(ListOfDomainTypes lodt){
-		checkList(lodt);
-	}
-	
-	private void checkCoordinateComponents(ListOfCoordinateComponents locc){
-		checkRequired(locc);
-		for(int i = 0 ; i < locc.size() ; i++){
-			CoordinateComponent cc = locc.get(i);
-			checkRequired(cc);
-			checkRequired(cc.getBoundaryMax());
-			checkRequired(cc.getBoundaryMin());
-		}
-	}
-	
-	private void checkRequired(SBase s){
-		if(!s.hasRequiredAttributes())
-			printError(s, "attribute ");
-		
-		if(!s.hasRequiredElements())
-			printError(s, "element ");
-	}
-	
-	private void checkList(ListOf lo){
-		for(int i = 0 ; i < lo.size() ; i++)
-			checkRequired(lo.get(i));
-	}
-	
-	private void printError(SBase s, String part){
-		String id = s.getId();
-		if(id.equals("")) id = s.getParentSBMLObject().getId();
-		IJ.log("missing required " +  part + "in " +  s.getClass() + " " + id + " at line: " + s.getLine());
-		errorFlag = true;
-	}
 	
 	public static void main(String[] args) {
 		SBMLReader reader = new SBMLReader();
 		SBMLDocument d = reader.readSBML("sampledfield_3d.xml");
-		ModelValidator mv = new ModelValidator(d.getModel());
+		ModelValidator mv = new ModelValidator(d);
 		mv.validate();
 	}
 
