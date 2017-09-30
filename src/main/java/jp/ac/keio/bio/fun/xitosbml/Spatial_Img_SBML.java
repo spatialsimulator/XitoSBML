3f5e38bfc1b1d99f2ad9966a80328dc21b7bff61
Akira Funahashi <funa@bio.keio.ac.jp>
2017-09-30 21:33:11 +0900

    Updated authors and version info. Add about menu

--- src/main/java/jp/ac/keio/bio/fun/xitosbml/Spatial_Img_SBML.java
+++ src/main/java/jp/ac/keio/bio/fun/xitosbml/Spatial_Img_SBML.java
@@ -14,10 +14,26 @@ public class Spatial_Img_SBML extends Spatial_SBML {
 	 * @see sbmlplugin.Spatial_SBML#run(java.lang.String)
 	 */
 	public void run(String args) {   
+	  if (args.equals("about")) {
+	    showAbout();
+	    return;
+	  } else {
 			new MainImgSpatial().run(args);	
+	  }
 	}
 	
-	
+	public void showAbout() {
+    IJ.showMessage("XitoSBML: Spatial SBML Plugin for ImageJ",
+        "XitoSBML is an ImageJ plugin which creates Spatial SBML model\n"
+        + "from segmented images. XitoSBML is not just a converter,\n"
+        + "but also a spatial model editor so that users can add\n"
+        + "molecules(species), reactions and advection/diffusion coefficients\n"
+        + "to the converted Spatial SBML model.\n"
+        + "More information is available at\n \n"
+        + "    "+"https://github.com/spatialsimulator/XitoSBML"
+        );
+  }
+
 	public static void main(String[] args) {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
 		Class<?> clazz = Spatial_Img_SBML.class;
