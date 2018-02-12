652113c92c1433a584cc77602d35e0b5a64083ab
kaitoii11 <kaitoii1111@gmail.com>
2018-02-13 00:26:34 +0900

    added boundary condition validation

--- src/main/java/jp/ac/keio/bio/fun/xitosbml/util/ModelValidator.java
+++ src/main/java/jp/ac/keio/bio/fun/xitosbml/util/ModelValidator.java
@@ -2,12 +2,30 @@ package jp.ac.keio.bio.fun.xitosbml.util;
 
 import ij.IJ;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import javax.xml.stream.XMLStreamException;
+
+import org.sbml.jsbml.ListOf;
 import org.sbml.jsbml.Model;
+import org.sbml.jsbml.Parameter;
 import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.SBMLError;
 import org.sbml.jsbml.SBMLErrorLog;
+import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.Species;
+import org.sbml.jsbml.ext.spatial.BoundaryCondition;
+import org.sbml.jsbml.ext.spatial.CoordinateComponent;
+import org.sbml.jsbml.ext.spatial.Geometry;
+import org.sbml.jsbml.ext.spatial.SpatialConstants;
+import org.sbml.jsbml.ext.spatial.SpatialModelPlugin;
+import org.sbml.jsbml.ext.spatial.SpatialParameterPlugin;
+import org.sbml.jsbml.ext.spatial.SpatialSpeciesPlugin;
 import org.sbml.jsbml.validator.SBMLValidator.CHECK_CATEGORY;
 
 
@@ -58,6 +76,69 @@ public class ModelValidator {
 		return document.getPackageRequired("spatial");
 	}
 	
+	private void checkMissingBourdariesConditions() {
+		SpatialModelPlugin spatialPlugin = (SpatialModelPlugin) model.getPlugin(SpatialConstants.namespaceURI);
+		Geometry geometry = spatialPlugin.getGeometry();
+		int dimensions = geometry.getListOfCoordinateComponents().size();
+		int maxBoundaryCondition = dimensions*2 + 1;
+		
+		ListOf<Parameter> lop = model.getListOfParameters();
+		HashMap<String,Set<String>> speciesBoundSet = new HashMap<String,Set<String>>(); 
+		for(Parameter p: lop) {
+			SpatialParameterPlugin spp = (SpatialParameterPlugin) p.getPlugin(SpatialConstants.namespaceURI);
+			if(spp.getParamType() instanceof BoundaryCondition) {
+				BoundaryCondition bc =  (BoundaryCondition) spp.getParamType();
+				
+				String speciesID = bc.getVariable();
+				String bound;
+				if(bc.isSetCoordinateBoundary())
+					bound = bc.getCoordinateBoundary();
+				else
+					bound = bc.getBoundaryDomainType();
+				
+				if(!speciesBoundSet.containsKey(speciesID)) {
+					Set<String> hash = new HashSet<String>();
+					hash.add(bound);
+					speciesBoundSet.put(speciesID, hash);
+				} else {
+					speciesBoundSet.get(speciesID).add(bound);
+				}
+			}
+		}
+		
+		ListOf<Species> los = model.getListOfSpecies();
+		ListOf<CoordinateComponent> locc = geometry.getListOfCoordinateComponents();
+		Set<String> coordSet = new HashSet<String>();
+		for(CoordinateComponent cc: locc) {
+			coordSet.add(cc.getBoundaryMaximum().getSpatialId());
+			coordSet.add(cc.getBoundaryMinimum().getSpatialId());
+		}
+		
+		for(Species s: los) {
+			SpatialSpeciesPlugin ssp = (SpatialSpeciesPlugin) s.getPlugin(SpatialConstants.namespaceURI);
+			if(ssp == null || !ssp.isSetSpatial())
+				continue;
+			
+			if( speciesBoundSet.containsKey(s.getId()) && speciesBoundSet.get(s.getId()).size() == maxBoundaryCondition)
+				continue;
+			
+			Set<String> speciesBound = speciesBoundSet.get(s.getId());
+			StringBuilder errorStr = new StringBuilder();
+			errorStr.append("Warning missing boundary condition for Species " + s.getId() + " on boundaries:");
+			
+			for(String str : coordSet) {
+				if(!speciesBound.contains(str))
+					errorStr.append(" " + str);
+			}
+
+			if(!speciesBound.contains(s.getCompartment()))
+				errorStr.append(" " + s.getCompartment());
+			
+			IJ.log(errorStr.toString());
+		}
+		
+	}
+	
 	/**
 	 * Validate.
 	 */
@@ -76,6 +157,20 @@ public class ModelValidator {
 		for (SBMLError e : errorList) {
 			IJ.log("Line " + e.getLine() + ": " + e.getMessage());
 		}
+		
+		checkMissingBourdariesConditions();
 	}
 
+	public static void main(String[] args) {
+		SBMLDocument d;
+			try {
+				d = SBMLReader.read(new File("/Users/ii/Desktop/model/analytic_3d.xml"));
+				new ModelValidator(d).validate();
+			} catch (XMLStreamException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	
+	}
 }
