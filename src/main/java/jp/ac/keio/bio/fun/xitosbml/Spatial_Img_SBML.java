25f22cf500a91161b998e13548686fd232691c6c
Akira Funahashi <funa@bio.keio.ac.jp>
2019-08-15 20:55:00 +0900

    Add comments.

--- src/main/java/jp/ac/keio/bio/fun/xitosbml/Spatial_Img_SBML.java
+++ src/main/java/jp/ac/keio/bio/fun/xitosbml/Spatial_Img_SBML.java
@@ -4,15 +4,17 @@ import ij.IJ;
 import ij.ImageJ;
 import jp.ac.keio.bio.fun.xitosbml.xitosbml.MainImgSpatial;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Spatial_Img_SBML.
+ * This class has several methods which will be able to launch from * 'Plugins' -> 'XitoSBML'.
+ * List of ImageJ plugins which XitoSBML provieds is described in src/main/resources/plugins.config.
  */
 public class Spatial_Img_SBML extends Spatial_SBML {
 
-	/* (non-Javadoc)
-	 * @see sbmlplugin.Spatial_SBML#run(java.lang.String)
-	 */
+  /**
+   * Launch XitoSBML as ImageJ plugin.
+   * List of ImageJ plugins which XitoSBML provieds is described in src/main/resources/plugins.config.
+   */
 	public void run(String args) {   
 	  if (args.equals("about")) {
 	    showAbout();
@@ -22,10 +24,13 @@ public class Spatial_Img_SBML extends Spatial_SBML {
 	  }
 	}
 	
+	/**
+	 * Display "About" message
+	 */
 	public void showAbout() {
     IJ.showMessage("XitoSBML",
         "XitoSBML " + this.version + ": Spatial SBML Plugin for ImageJ\n"
-        + "Copyright (C) 2014-2017 Funahashi Lab. Keio University.\n \n"
+        + "Copyright (C) 2014-2019 Funahashi Lab. Keio University.\n \n"
         + "XitoSBML is an ImageJ plugin which creates Spatial SBML model\n"
         + "from segmented images. XitoSBML is not just a converter,\n"
         + "but also a spatial model editor so that users can add\n"
@@ -36,6 +41,10 @@ public class Spatial_Img_SBML extends Spatial_SBML {
         );
   }
 
+	/**
+	 * Example main() method which will launch ImageJ and call XitoSBML.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
 		Class<?> clazz = Spatial_Img_SBML.class;
